fef7cdc60e848df323763cec91aeb33af61b9ab8
Donna Malayeri <donnam@microsoft.com>
2015-10-31 21:25:57 -0700

    Post to slack & add NH api call when job is completed

--- server/CarDealership/Controllers/JobController.cs
+++ server/CarDealership/Controllers/JobController.cs
@@ -6,6 +6,12 @@ using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using FieldEngineerLiteService.DataObjects;
 using FieldEngineerLiteService.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using System.Configuration;
+using System;
+using System.Diagnostics;
 
 namespace FieldEngineerLiteService.Controllers
 {
@@ -33,26 +39,49 @@ namespace FieldEngineerLiteService.Controllers
         // PATCH tables/Job/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public async Task<Job> PatchJob(string id, Delta<Job> patch)
         {
+            var updatedJob = await UpdateAsync(id, patch);
 
-            Job job = patch.GetEntity();  // get new value
-            //var user = this.User as MobileAppUser;
-            //var creds = await user.GetIdentityAsync<AzureActiveDirectoryCredentials>();
-            //var token = this.Request.Headers.GetValues("x-zumo-auth").First();
-            //SalesforceClient client = new SalesforceClient(false);
-            //client.SetUser(user.Id, token);
-            //ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
-            //traceWriter.Info("Patch: userId: " + user.Id + ", token:" + token);            
+            if (updatedJob.Status == "Completed") {
+                await SendNotifications(updatedJob);
+            }
 
-            //await client.UpdateCase( "0000" + job.JobNumber, job.Status, job.WorkPerformed);
-
-            return await UpdateAsync(id, patch);
+            return updatedJob;
         }
 
         // POST tables/Job
         public async Task<IHttpActionResult> PostJob(Job item)
         {
             Job current = await InsertAsync(item);
+
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
+        }
+
+
+        private async Task SendJsonAsync(string requestUri, string key, string value)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var contentBody = new JObject();
+            contentBody[key] = value;
+
+            await client.PostAsJsonAsync(requestUri, contentBody);
+        }
+
+        private async Task SendNotifications(Job job)
+        {
+            string slackUri = ConfigurationManager.AppSettings["SLACK_CustomerNotificationsHook"];
+            string notifyUri = ConfigurationManager.AppSettings["SendNotificationUri"];
+
+            try {
+                var slackString = String.Format("Job for customer {0} was completed: {1}", job.CustomerName, job.Title);
+                await SendJsonAsync(slackUri, "text", slackString);
+
+                await SendJsonAsync(notifyUri, "toast", "Your repair request has been closed. Thank you.");
+            }
+            catch (Exception e) {
+                Trace.WriteLine(e);
+            }
         }
 
         // DELETE tables/Job/48D68C86-6EA6-4C25-AA33-223FC9A27959
